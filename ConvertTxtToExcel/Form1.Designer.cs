2dc17a3b65970cae1b036e24c62b8c0c4aeb2a0b
Javad Noruzi <jnoruzi9799@gmail.com>
2025-04-22 19:21:34 +0330

    init

---
+++ ConvertTxtToExcel/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace ConvertTxtToExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txt_OutPutPath = new TextBox();
+            lbl_Path = new Label();
+            lbl_TextMain = new Label();
+            lbl_Spliter = new Label();
+            txt_Spliter = new TextBox();
+            btn_Convert = new Button();
+            lbl_Formats = new Label();
+            com_Formats = new ComboBox();
+            txt_inputText = new TextBox();
+            openFileDialog1 = new OpenFileDialog();
+            btn_SelectFile = new Button();
+            btn_SelectPath = new Button();
+            saveFileDialog1 = new SaveFileDialog();
+            SuspendLayout();
+            // 
+            // txt_OutPutPath
+            // 
+            txt_OutPutPath.Location = new Point(115, 55);
+            txt_OutPutPath.Name = "txt_OutPutPath";
+            txt_OutPutPath.Size = new Size(733, 30);
+            txt_OutPutPath.TabIndex = 0;
+            // 
+            // lbl_Path
+            // 
+            lbl_Path.AutoSize = true;
+            lbl_Path.Location = new Point(-6, 55);
+            lbl_Path.Name = "lbl_Path";
+            lbl_Path.Size = new Size(104, 23);
+            lbl_Path.TabIndex = 1;
+            lbl_Path.Text = "OutPut Path";
+            // 
+            // lbl_TextMain
+            // 
+            lbl_TextMain.AutoSize = true;
+            lbl_TextMain.Location = new Point(13, 22);
+            lbl_TextMain.Name = "lbl_TextMain";
+            lbl_TextMain.Size = new Size(85, 23);
+            lbl_TextMain.TabIndex = 2;
+            lbl_TextMain.Text = "InputPath";
+            // 
+            // lbl_Spliter
+            // 
+            lbl_Spliter.AutoSize = true;
+            lbl_Spliter.Location = new Point(40, 110);
+            lbl_Spliter.Name = "lbl_Spliter";
+            lbl_Spliter.Size = new Size(58, 23);
+            lbl_Spliter.TabIndex = 4;
+            lbl_Spliter.Text = "Spliter";
+            // 
+            // txt_Spliter
+            // 
+            txt_Spliter.Location = new Point(115, 105);
+            txt_Spliter.Name = "txt_Spliter";
+            txt_Spliter.Size = new Size(95, 30);
+            txt_Spliter.TabIndex = 5;
+            txt_Spliter.Text = "\"|\"";
+            // 
+            // btn_Convert
+            // 
+            btn_Convert.BackColor = SystemColors.ActiveCaption;
+            btn_Convert.Location = new Point(842, 99);
+            btn_Convert.Name = "btn_Convert";
+            btn_Convert.Size = new Size(155, 42);
+            btn_Convert.TabIndex = 6;
+            btn_Convert.Text = "Convert";
+            btn_Convert.UseVisualStyleBackColor = false;
+            btn_Convert.Click += btn_Convert_Click;
+            // 
+            // lbl_Formats
+            // 
+            lbl_Formats.AutoSize = true;
+            lbl_Formats.Location = new Point(216, 109);
+            lbl_Formats.Name = "lbl_Formats";
+            lbl_Formats.Size = new Size(64, 23);
+            lbl_Formats.TabIndex = 7;
+            lbl_Formats.Text = "Format";
+            // 
+            // com_Formats
+            // 
+            com_Formats.FormattingEnabled = true;
+            com_Formats.Location = new Point(281, 107);
+            com_Formats.Name = "com_Formats";
+            com_Formats.Size = new Size(550, 31);
+            com_Formats.TabIndex = 8;
+            // 
+            // txt_inputText
+            // 
+            txt_inputText.Location = new Point(115, 19);
+            txt_inputText.Name = "txt_inputText";
+            txt_inputText.Size = new Size(733, 30);
+            txt_inputText.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btn_SelectFile
+            // 
+            btn_SelectFile.Location = new Point(854, 19);
+            btn_SelectFile.Name = "btn_SelectFile";
+            btn_SelectFile.Size = new Size(126, 34);
+            btn_SelectFile.TabIndex = 9;
+            btn_SelectFile.Text = "Select File";
+            btn_SelectFile.UseVisualStyleBackColor = true;
+            btn_SelectFile.Click += btn_SelectFile_Click;
+            // 
+            // btn_SelectPath
+            // 
+            btn_SelectPath.Location = new Point(854, 55);
+            btn_SelectPath.Name = "btn_SelectPath";
+            btn_SelectPath.Size = new Size(126, 34);
+            btn_SelectPath.TabIndex = 9;
+            btn_SelectPath.Text = "Select Path";
+            btn_SelectPath.UseVisualStyleBackColor = true;
+            btn_SelectPath.Click += btn_SelectPath_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1003, 165);
+            Controls.Add(btn_SelectPath);
+            Controls.Add(btn_SelectFile);
+            Controls.Add(com_Formats);
+            Controls.Add(lbl_Formats);
+            Controls.Add(btn_Convert);
+            Controls.Add(txt_Spliter);
+            Controls.Add(lbl_Spliter);
+            Controls.Add(lbl_TextMain);
+            Controls.Add(lbl_Path);
+            Controls.Add(txt_inputText);
+            Controls.Add(txt_OutPutPath);
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Text Converter";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txt_OutPutPath;
+        private Label lbl_Path;
+        private Label lbl_TextMain;
+        private Label lbl_Spliter;
+        private TextBox txt_Spliter;
+        private Button btn_Convert;
+        private Label lbl_Formats;
+        private ComboBox com_Formats;
+        private TextBox txt_inputText;
+        private OpenFileDialog openFileDialog1;
+        private Button btn_SelectFile;
+        private Button btn_SelectPath;
+        private SaveFileDialog saveFileDialog1;
+    }
+}
---
+++ ConvertTxtToExcel/Form1.cs
@@ -0,0 +1,167 @@
+﻿using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+
+namespace ConvertTxtToExcel
+{
+    public partial class Form1 : Form
+    {
+        public string FileName { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+            IntializeFormat();
+        }
+
+        private void btn_Convert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string inputPath = txt_inputText.Text.Trim();
+                string outputPath = txt_OutPutPath.Text.Trim();
+                string spliter = txt_Spliter.Text.Trim();
+                if (!string.IsNullOrEmpty(inputPath))
+                {
+                    string format = com_Formats.Text;
+                    FileName = Path.GetFileName(inputPath);
+                    switch (format)
+                    {
+                        case "Excel 97-2003":
+                            ConvertToExcel97(inputPath, outputPath, spliter);
+                            break;
+
+                        default:
+
+                            break;
+
+
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Text is Empty!");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public void IntializeFormat()
+        {
+            try
+            {
+                com_Formats.Items.Add("Excel 97-2003");
+                com_Formats.SelectedItem = "Excel 97-2003";
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show($"Error Add Format To ComboBox Error:{ex.Message}");
+            }
+        }
+        public void ConvertToExcel97(string inputPath, string outputPath, string spliter)
+        {
+
+            try
+            {
+                // بررسی وجود فایل ورودی
+                if (!File.Exists(inputPath))
+                {
+                    MessageBox.Show("فایل ورودی وجود ندارد!");
+                    return;
+                }
+
+                // بررسی وجود پوشه مقصد و ساخت آن در صورت نیاز
+                string directory = Path.GetDirectoryName(outputPath);
+                FileName=Path.GetFileName(inputPath);
+                if (!Directory.Exists(directory))
+                {
+                    Directory.CreateDirectory(directory);
+                }
+
+                // ساخت Workbook و Sheet
+                HSSFWorkbook workbook = new HSSFWorkbook();
+                ISheet sheet = workbook.CreateSheet("Sheet1");
+
+                var lines = File.ReadAllLines(inputPath);
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    var row = sheet.CreateRow(i);
+
+                    // اگر اسپلیتر بیشتر از یک کاراکتر است، استفاده از StringSplitOptions.None
+                    var cells = lines[i].Split(new string[] { spliter }, StringSplitOptions.None);
+
+                    for (int j = 0; j < cells.Length; j++)
+                    {
+                        row.CreateCell(j).SetCellValue(cells[j]);
+                    }
+                }
+
+                // ذخیره در فایل Excel
+                using (var fs = new FileStream(outputPath, FileMode.Create, FileAccess.Write))
+                {
+                    workbook.Write(fs);
+                }
+
+                MessageBox.Show("تبدیل با موفقیت انجام شد! ✅", "Excel Export", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (UnauthorizedAccessException uaEx)
+            {
+                MessageBox.Show($"دسترسی به مسیر مورد نظر وجود ندارد!\n\n{uaEx.Message}", "خطای دسترسی", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"خطا در تبدیل به فرمت Excel 97-2003:\n\n{ex.Message}", "خطا", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btn_SelectFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                {
+                    openFileDialog.Title = "انتخاب فایل";
+                    openFileDialog.Filter = "All Files (*.*)|*.*"; // می‌تونی فیلتر رو تغییر بدی مثلا فقط txt
+
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        // مسیر کامل فایل انتخاب شده رو می‌ذاره توی TextBox
+                        txt_inputText.Text = openFileDialog.FileName;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn_SelectPath_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string inputPath = txt_inputText.Text.Trim();
+                FileName = Path.GetFileName(inputPath).Split('.')[0];
+                using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                {
+                    saveFileDialog.Title = "مسیر ذخیره‌سازی فایل را انتخاب کنید";
+                    saveFileDialog.Filter = "Excel 97-2003 (*.xls)|*.xls|All Files (*.*)|*.*";
+                    saveFileDialog.DefaultExt = "xls";
+                    saveFileDialog.FileName = $"{FileName}.xls"; // اسم پیش‌فرض فایل
+
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        txt_OutPutPath.Text = saveFileDialog.FileName;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
